84f0361efb9fa2251aaefe7ee709412458921231
a03pethe <a03pethe@gmail.com>
2012-05-29 17:35:41 +0200

    forgot to add the new files.

---
+++ SQLRepeater.Entities/OptionEntities/ExecutionTaskOptions.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLRepeater.Entities.OptionEntities
+{
+    public class ExecutionTaskOptions : EntityBase
+    {
+
+
+        static readonly ExecutionTaskOptions _instance = new ExecutionTaskOptions();
+
+        public static ExecutionTaskOptions Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        static ExecutionTaskOptions()
+        {        
+        }
+
+        ExecutionTaskOptions()
+        {
+            
+        }
+
+
+        bool _onlyOutputToFile = true;
+        public bool OnlyOutputToFile
+        {
+            get
+            {
+                return _onlyOutputToFile;
+            }
+            set
+            {
+                if (_onlyOutputToFile != value)
+                {
+                    _onlyOutputToFile = value;
+                    OnPropertyChanged("OnlyOutputToFile");
+                }
+            }
+        }
+
+
+        long _startValue = 1;
+        public long StartValue
+        {
+            get
+            {
+                return _startValue;
+            }
+            set
+            {
+                if (_startValue != value)
+                {
+                    _startValue = value;
+                    OnPropertyChanged("StartValue");
+                }
+            }
+        }
+
+        DateTime _dateTimeGenerationStartTime = DateTime.Now;
+        public DateTime DateTimeGenerationStartTime
+        {
+            get
+            {
+                return _dateTimeGenerationStartTime;
+            }
+            set
+            {
+                if (_dateTimeGenerationStartTime != value)
+                {
+                    _dateTimeGenerationStartTime = value;
+                    OnPropertyChanged("DateTimeGenerationStartTime");
+                }
+            }
+        }
+
+
+        int _fixedExecutions = 10;
+        public int FixedExecutions
+        {
+            get
+            {
+                return _fixedExecutions;
+            }
+            set
+            {
+                if (_fixedExecutions != value)
+                {
+                    _fixedExecutions = value;
+                    OnPropertyChanged("FixedExecutions");
+                }
+            }
+        }
+
+        ExecutionTypes _executionType = ExecutionTypes.DurationBased;
+        public ExecutionTypes ExecutionType
+        {
+            get
+            {
+                return _executionType;
+            }
+            set
+            {
+                if (_executionType != value)
+                {
+                    _executionType = value;
+                    OnPropertyChanged("ExecutionType");
+                }
+            }
+        }
+
+       
+        int _secondsToRun = 2;
+        public int SecondsToRun
+        {
+            get
+            {
+                return _secondsToRun;
+            }
+            set
+            {
+                if (_secondsToRun != value)
+                {
+                    _secondsToRun = value;
+                    OnPropertyChanged("SecondsToRun");
+                }
+            }
+        }
+
+        int _maxThreads = 1;
+        public int MaxThreads
+        {
+            get
+            {
+                return _maxThreads;
+            }
+            set
+            {
+                if (_maxThreads != value)
+                {
+                    _maxThreads = value;
+                    OnPropertyChanged("MaxThreads");
+                }
+            }
+        }
+
+
+        
+       
+    }
+
+    public enum ExecutionTypes
+    {
+        DurationBased,
+        ExecutionCountBased
+    } 
+}
