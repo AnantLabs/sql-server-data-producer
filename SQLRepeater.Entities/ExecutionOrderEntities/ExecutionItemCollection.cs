c635d8f0f99a000126a9085a80cba9a6ee68059f
a03pethe <a03pethe@gmail.com>
2012-06-01 18:15:36 +0200

    Adding ExecutionItemCollection class to hold a group of ExecutionItems. WIP

---
+++ SQLRepeater.Entities/ExecutionOrderEntities/ExecutionItemCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace SQLRepeater.Entities.ExecutionOrderEntities
+{
+    public class ExecutionItemCollection : ObservableCollection<ExecutionItem>
+    {
+
+        public ExecutionItemCollection()
+            : base()
+        {
+            
+        }
+
+        string _collectionName;
+        public string CollectionName
+        {
+            get
+            {
+                return _collectionName;
+            }
+            set
+            {
+                if (_collectionName != value)
+                {
+                    _collectionName = value;
+                    OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("CollectionName"));
+                }
+            }
+        }
+
+
+        object _executionGroupOptions;
+        public object ExecutionGroupOptions
+        {
+            get
+            {
+                return _executionGroupOptions;
+            }
+            set
+            {
+                if (_executionGroupOptions != value)
+                {
+                    _executionGroupOptions = value;
+                    OnPropertyChanged(new System.ComponentModel.PropertyChangedEventArgs("ExecutionGroupOptions"));
+                }
+            }
+        }
+
+    }
+}
