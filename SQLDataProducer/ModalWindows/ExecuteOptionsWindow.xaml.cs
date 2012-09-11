624866757baae5beaae63848ede2e0341210d5d8
a03pethe <a03pethe@gmail.com>
2012-09-11 17:19:55 +0200

    Preparing for having the Execute Window as a modal window, that pop up when you want to run

---
+++ SQLDataProducer/ModalWindows/ExecuteOptionsWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using SQLDataProducer.Model;
+
+namespace SQLDataProducer.ModalWindows
+{
+    /// <summary>
+    /// Interaction logic for ExecuteOptionsWindow.xaml
+    /// </summary>
+    public partial class ExecuteOptionsWindow : Window
+    {
+        ApplicationModel _model;
+        public ExecuteOptionsWindow(ApplicationModel model)
+        {
+            _model = model;
+            Loaded += new RoutedEventHandler(ExecuteOptionsWindow_Loaded);
+            InitializeComponent();
+        }
+
+        void ExecuteOptionsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = _model; 
+        }
+    }
+}
