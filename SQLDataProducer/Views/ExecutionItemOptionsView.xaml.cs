505fd78300a3ca02973a5fd9cb26a7fe5e06f180
a03pethe <a03pethe@gmail.com>
2012-12-10 11:18:36 +0100

    Improving layout.
    Removed unwanted text from Countries.txt

---
+++ SQLDataProducer/Views/ExecutionItemOptionsView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLDataProducer.Views
+{
+    /// <summary>
+    /// Interaction logic for ExecutionItemOptionsView.xaml
+    /// </summary>
+    public partial class ExecutionItemOptionsView : UserControl
+    {
+        public ExecutionItemOptionsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
