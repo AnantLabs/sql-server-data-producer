341ac4e3fdc13b54e2b455a25ef066706dae69bf
Peter Henell <mail@peterhenell.se>
2013-12-28 13:12:33 +0100

    Remove unused code, added comments about stuff TODO

--- SQLDataProducer.Entities/Generators/GeneratorFactory.cs
+++ SQLDataProducer.Entities/Generators/GeneratorFactory.cs
@@ -41,98 +41,15 @@ namespace SQLDataProducer.Entities.Generators
 
             if (column.IsIdentity)
             {
+                // TODO: Fix hardcoded INT generator, this is sort of safe since we are only checking the name but it is a smell.
+                // TODO: sorting by using != is not intuitive
                 gens = new ObservableCollection<AbstractValueGenerator>(gens.OrderBy(x => x.GeneratorName != IdentityIntGenerator.GENERATOR_NAME));
             }
 
             return gens;
         }
 
-        //public static ObservableCollection<Generator> GetGeneratorsForDataType(ColumnDataTypeDefinition dbataTypeDef)
-        //{
-        //    ObservableCollection<Generator> gens = GetDefaultGeneratorsForDataType(dbataTypeDef);
-
-        //    // If the column is nullable then the default generator should be the NULL generator. We insert it at the top so that it will be picked up as the default.
-        //    if (dbataTypeDef.IsNullable)
-        //        gens.Insert(0, Generator.CreateNULLValueGenerator());
-
-        //    return gens;
-        //}
-
-        //private static ObservableCollection<Generator> GetDefaultGeneratorsForDataType(ColumnDataTypeDefinition dbataTypeDef)
-        //{
-        //    switch (dbataTypeDef.DBType)
-        //    {
-        //        case System.Data.SqlDbType.BigInt:
-        //            return Generators.Generator.GetGeneratorsForBigInt();
-
-        //        case System.Data.SqlDbType.Bit:
-        //            return Generators.Generator.GetGeneratorsForBit();
-        //        case System.Data.SqlDbType.Char:
-        //            return Generators.Generator.GetStringGenerators(dbataTypeDef.MaxLength);
-
-        //        case System.Data.SqlDbType.Date:
-        //        case System.Data.SqlDbType.DateTime:
-        //        case System.Data.SqlDbType.DateTime2:
-        //        case System.Data.SqlDbType.SmallDateTime:
-        //        case System.Data.SqlDbType.Time:
-        //        case System.Data.SqlDbType.DateTimeOffset:
-        //            return Generators.Generator.GetDateTimeGenerators();
-
-        //        case System.Data.SqlDbType.Decimal:
-        //            return Generators.Generator.GetDecimalGenerators(100000000000000000);
-        //        case System.Data.SqlDbType.Float:
-        //            return Generators.Generator.GetDecimalGenerators(100000000000000000);
-        //        case System.Data.SqlDbType.Real:
-        //            return Generators.Generator.GetDecimalGenerators(100000000000000000);
-        //        case System.Data.SqlDbType.Money:
-        //            return Generators.Generator.GetDecimalGenerators(922337203685470);
-        //        case System.Data.SqlDbType.SmallMoney:
-        //            return Generators.Generator.GetDecimalGenerators(214740);
-
-
-        //        case System.Data.SqlDbType.Binary:
-        //        case System.Data.SqlDbType.Image:
-        //        case System.Data.SqlDbType.Timestamp:
-        //        case System.Data.SqlDbType.VarBinary:
-        //            return Generators.Generator.GetBinaryGenerators(1);
-
-        //        case System.Data.SqlDbType.Int:
-        //            return Generators.Generator.GetGeneratorsForInt();
-
-        //        case System.Data.SqlDbType.NChar:
-        //        case System.Data.SqlDbType.NText:
-        //        case System.Data.SqlDbType.NVarChar:
-        //        case System.Data.SqlDbType.Text:
-        //        case System.Data.SqlDbType.VarChar:
-        //            return Generators.Generator.GetStringGenerators(dbataTypeDef.MaxLength);
-
-        //        case System.Data.SqlDbType.SmallInt:
-        //            return Generators.Generator.GetGeneratorsForSmallInt();
-
-        //        case System.Data.SqlDbType.Structured:
-        //            break;
-        //        case System.Data.SqlDbType.TinyInt:
-        //            return Generators.Generator.GetGeneratorsForTinyInt();
-        //        case System.Data.SqlDbType.Udt:
-        //            break;
-        //        case System.Data.SqlDbType.UniqueIdentifier:
-        //            return Generators.Generator.GetGUIDGenerators();
-
-        //        case System.Data.SqlDbType.Variant:
-        //            return Generators.Generator.GetGeneratorsForInt();
-
-        //        case System.Data.SqlDbType.Xml:
-        //            return Generators.Generator.GetXMLGenerators();
-
-        //        default:
-        //            break;
-        //    }
-
-        //    return new ObservableCollection<Generator>();
-
-        //}
-
-        public static List<AbstractValueGenerator> GetForeignKeyGenerators(ObservableCollection<string> fkKeys)
+        public static List<AbstractValueGenerator> GetForeignKeyGenerators(IEnumerable<string> fkKeys)
         {
             var l = new System.Collections.Generic.List<AbstractValueGenerator>();
             //l.Add(Generators.Generator.CreateRandomForeignKeyGenerator(fkKeys));
